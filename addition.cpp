826f8699921189924003b1ca622aadb4c25dd9d0
TONNIE9 <99641784+TONNIE9@users.noreply.github.com>
2022-03-21 10:10:10 +0300

    init

---
+++ addition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,sum;
+	cout<<"Enter value of a"<<endl;
+	cin>>a;
+		cout<<"Enter value of b"<<endl;
+	cin>>b;
+		cout<<"Enter value of c"<<endl;
+	cin>>c;
+	sum=a+b+c;
+	cout<<a<<"+"<<b<<"+"<<c<<"="<<sum<<endl;
+	return 0;
+}
