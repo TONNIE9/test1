cc60c4076d71ac127d848eec7eecae299c283050
TONNIE9 <99641784+TONNIE9@users.noreply.github.com>
2022-03-21 10:15:49 +0300

    init

--- addition.cpp
+++ addition.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a,b,c,sum;
-	cout<<"Enter value of a"<<endl;
+	int e,f,g,sum;
+	cout<<"Enter value of e"<<endl;
 	cin>>a;
-		cout<<"Enter value of b"<<endl;
+		cout<<"Enter value of f"<<endl;
 	cin>>b;
-		cout<<"Enter value of c"<<endl;
+		cout<<"Enter value of g"<<endl;
 	cin>>c;
 	sum=a+b+c;
-	cout<<a<<"+"<<b<<"+"<<c<<"="<<sum<<endl;
+	cout<<e<<"+"<<f<<"+"<<g<<"="<<sum<<endl;
 	return 0;
 }
